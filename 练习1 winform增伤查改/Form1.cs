042e66fa296880a3c806ae6e37d5da6aa88c83d2
Hans Yang <545872067@qq.com>
2018-10-31 22:46:05 +0800

    完善sqlhelper

---
+++ 练习1 winform增伤查改/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+
+namespace 练习1_winform增伤查改
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //连接字符串
+            string connStr = ConfigurationManager.ConnectionStrings["sql"].ConnectionString;
+
+            //sql语句
+            string sql = "select * from StuTest";
+
+            //数据集
+            DataSet ds = new DataSet();
+
+            //准备适配器
+            using (SqlDataAdapter sda = new SqlDataAdapter(sql, connStr))
+            {
+                sda.Fill(ds);
+            }
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ///导出的两个方法
+
+            #region 
+            ///使用StreamWriter 导出 到txt文件   方法一：
+
+            //写入到文本文件
+            //Stopwatch sp = new Stopwatch();
+            //sp.Start();
+            //string connStr = ConfigurationManager.ConnectionStrings["sql"].ConnectionString;
+
+            //string sql = "select * from StuTest";
+            //StreamWriter writer = new StreamWriter("student.txt", true, Encoding.Default);
+
+            //string conStr = ConfigurationManager.ConnectionStrings["sql"].ConnectionString;
+
+            //using (SqlConnection conn = new SqlConnection(conStr))
+            //{
+            //    using (SqlCommand cmd = new SqlCommand(sql, conn))
+            //    {
+            //        conn.Open();
+            //        using (SqlDataReader reader = cmd.ExecuteReader())
+            //        {
+            //            while (reader.Read())
+            //            {
+            //                //获得数据，写入文件
+            //                List<string> list1 = new List<string>();
+
+            //                for (int i = 0; i < reader.FieldCount; i++)
+            //                {
+            //                    list1.Add(reader[i].ToString());
+            //                }
+            //                writer.WriteLine(string.Join(",", list1));
+            //            }
+            //        }
+            //        writer.Dispose();
+            //        sp.Stop();
+            //        MessageBox.Show("导出完成");
+            //    }
+
+            //}
+            #endregion
+
+            #region 
+            ///方法二： 通过 File 导出数据
+            Stopwatch sp = new Stopwatch();
+            sp.Start();
+
+            string sql="select * from StuTest";
+            string connStr = ConfigurationManager.ConnectionStrings["sql"].ConnectionString;
+
+            using (SqlConnection conn=new SqlConnection(connStr))
+            {
+                using (SqlCommand cmd=new SqlCommand(sql,conn))
+                {
+                    conn.Open();
+                    using (SqlDataReader reader=cmd.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            List<string> list1 = new List<string>();
+                            for(int i=0;i<reader.FieldCount;i++)
+                            {
+                                list1.Add(reader[i].ToString());
+                            }
+                            File.AppendAllText("tttttext.txt",string.Join(",",list1)+ "\n"+Encoding.Default);
+                        }
+                    }
+
+
+                }
+
+            }
+
+                sp.Stop();
+            MessageBox.Show("导出完成");
+           
+            #endregion
+        }
+    }
+}
--- 练习2 sqlhelper/Program.cs
+++ 练习2 sqlhelper/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace 练习2_sqlhelper
 {
@@ -10,6 +13,94 @@ namespace 练习2_sqlhelper
     {
         static void Main(string[] args)
         {
+            //1.插入数据
+
+            //（1)无参数 插入数据
+            //string sql = "insert into StuTest(stuid,stuname,pwd)values(1,'hans','aaa')";
+
+            //int result = SQLHelper.ExecuteNonQuery(sql);
+            //Console.WriteLine(result);
+
+
+            //(2)参数化  添加
+            //string sql = "insert into StuTest (stuid,stuname,pwd)values(@iddd,@nameee,@pwddd);";
+
+            //SqlParameter[] ps = {
+            //     new SqlParameter("@iddd",(object)2),
+            //     new SqlParameter("@nameee","Tom"),
+            //     new SqlParameter("@pwddd","ddd")
+
+            //};
+            //int result = SQLHelper.ExecuteNonQuery(sql,ps);
+            //Console.WriteLine(result);
+
+            ////2.修改数据 ,(参数化修改)
+            //string sql = "update StuTest set stuname=@nameee where stuid=@iddd";
+
+            //SqlParameter[] ps = {
+            //    new SqlParameter("@nameee","比尔盖茨"),
+            //    new SqlParameter("@iddd",(object)2)
+            //};
+
+            //int res = SQLHelper.ExecuteNonQuery(sql,ps);
+            //Console.WriteLine(res);
+
+
+
+            //3. 删除数据 
+            //string sql = "delete from StuTest where stuid=@iddd";
+            //SqlParameter pID = new SqlParameter("@iddd",(object)5);
+            //int count = SQLHelper.ExecuteNonQuery(sql,pID);
+            //Console.WriteLine(count);
+
+
+            //4.模糊查询，姓杨的多少人
+            //string sql = "select count(*) from StuTest where stuname  like @nameee";
+
+            //SqlParameter pName = new SqlParameter("@nameee","杨%");
+
+            //int count = (int)SQLHelper.ExecuteScalar(sql,pName);
+            //Console.WriteLine(count);
+
+
+            //5. 读取数据 datareader 方法
+            //using (SqlDataReader reader=SQLHelper.ExecuteReader("select * from Stutest"))
+            //{
+            //    List<string> list1 = new List<string>();
+            //    while (reader.Read())
+            //    {
+
+            //        for(int i=0;i<reader.FieldCount;i++)
+            //        {
+            //            list1.Add(reader[i].ToString());
+            //        }                                                     
+            //    }
+            //    foreach(var item in list1)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}                   
+            //Console.ReadKey();
+
+            //读取数据 方法2 通过streamwriter 写进txt文件 
+            string sql = "select * from StuTest";
+            using (StreamWriter writer=new StreamWriter("test.txt",true,Encoding.Default))
+            {
+                using (SqlDataReader reader=SQLHelper.ExecuteReader(sql))
+                {
+                    List<string> list1 = new List<string>();
+                    while(reader.Read())
+                    {
+                        list1.Clear();
+                        for(int i=0;i<reader.FieldCount;i++)
+                        {
+                            list1.Add(reader[i].ToString());
+                        }
+                        writer.WriteLine(string.Join(",",list1));
+                    }
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
